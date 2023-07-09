c72850572db6cd3f8a6663927af412353c61919f
Magda <magdagvirjishvili@gmail.com>
2023-07-10 00:50:31 +0400

    magdas_commit

---
+++ RectangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("calculate rectangle area");
+
+        Console.Write("enter the length  ");
+        double length = double.Parse(Console.ReadLine());
+
+        Console.Write("enter the width  ");
+        double width = double.Parse(Console.ReadLine());
+
+        double area = length * width;
+        Console.WriteLine("the rectangle area equals to " + area);
+    }
+}
+
+
+    
